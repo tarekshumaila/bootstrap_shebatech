0923195f19821adb6117900487c96112167acbf9
tarekshumaila <tarek@Tareks-MacBook-Air.local>
2015-04-22 12:36:41 -0600

    some more edits to the menu.. looking good

--- php/nav.php
+++ php/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-default navbar-fixed-top" id='navigationbar'>
-  <div class="container">
+  <div id='container' class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
         <span class="sr-only">Toggle navigation</span>
@@ -12,9 +12,9 @@
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav navbar-right">
         <li class="active" ><a id="home" href="#">Home</a></li>
-        <li><a href="#about">About</a></li>
+        <li><a id="aboutus" href="#about">About</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Services <span class="caret"></span></a>
+          <a id="services" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Services <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
             <li><a href="#">Web Development</a></li>
             <li><a href="#">Mobile Development</a></li>
@@ -31,9 +31,8 @@
             <li><a href="#">Supplier Agreements & Negotiation</a></li>
           </ul>
         </li>
-        <li><a href="#promotions">Promotions</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a id="promotions" href="#promotions">Promotions</a></li>
+        <li><a id="portfolio" href="#portfolio">Portfolio</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div>
